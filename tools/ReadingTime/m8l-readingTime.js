1b42e2be820bce35f1a009da8df5622cfcb9ec19
juan <juan@meaningfulteam.com>
2023-03-23 18:13:32 -0400

    Update: readingTime.js file

--- tools/ReadingTime/m8l-readingTime.js
+++ tools/ReadingTime/m8l-readingTime.js
@@ -1,27 +1,4 @@
-const rtConsoleProps = {
-    text_colors: {
-        Default: "black",
-        Error: "white",
-        Context: "white",
-    },
-    bg_colors: {
-        Default: "transparent",
-        Error: "red",
-        Context: "green",
-    },
-    types: {
-        Error: "Error",
-        Context: "Context",
-    },
-};
-
-const rtConsoleMsg = (category, msg) => {
-    console.log(
-        `%c${category}:%c ${msg}`,
-        `color: ${rtConsoleProps.text_colors[category]}; background-color: ${rtConsoleProps.bg_colors[category]}; font-size: larger; padding: 0rem 1rem`,
-        `color: ${rtConsoleProps.text_colors.default}`
-    );
-};
+import { consoleProps, consoleMsg } from "../common/m8l-consoleMsg.js";
 
 const getReadingTime = (type = "cumulative", speed = 200) => {
     try {
@@ -62,25 +39,27 @@ const getReadingTime = (type = "cumulative", speed = 200) => {
             display.innerText = `${Math.round(totalTime)} min`;
         });
     } catch (error) {
-        rtConsoleMsg(
-            rtConsoleProps.types.Error,
+        consoleMsg(
+            consoleProps.types.Error,
             `Error trying to calculate reading time. Error message → ${error}`
         );
     }
 };
 
-try {
-    var m8lConfig = m8lConfig || {};
-    if (m8lConfig["readingTime"]) {
-        try {
-            getReadingTime(
-                m8lConfig["readingTime"].calculationType,
-                m8lConfig["readingTime"].wordsPerMinute
-            );
-        } catch (err) {
-            throw new Error("Reading Time Script");
+document.addEventListener("DOMContentLoaded", function () {
+    try {
+        var m8lConfig = m8lConfig || {};
+        if (m8lConfig["readingTime"]) {
+            try {
+                getReadingTime(
+                    m8lConfig["readingTime"].calculationType,
+                    m8lConfig["readingTime"].wordsPerMinute
+                );
+            } catch (err) {
+                throw new Error("Reading Time Script");
+            }
         }
+    } catch (error) {
+        consoleMsg(consoleProps.types.Error, `Implementation '${error}'`);
     }
-} catch (error) {
-    rtConsoleMsg(rtConsoleProps.types.Error, `Implementation '${error}'`);
-}
+});
